0ce64c2e9366ffa737e8f8642edef30eff91c38c
alwxav98 <107285163+alwxav98@users.noreply.github.com>
2024-11-11 01:30:48 -0500

    Edit css

--- AplicacionWeb1/Program.cs
+++ AplicacionWeb1/Program.cs
@@ -6,11 +6,12 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseStaticFiles();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
-app.UseStaticFiles();
+
 
 app.UseRouting();
 
--- AplicacionWeb1/obj/Debug/net6.0/AplicacionWeb1.AssemblyInfo.cs
+++ AplicacionWeb1/obj/Debug/net6.0/AplicacionWeb1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicacionWeb1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+608caa0d4ec1959b54913e7570e8f00cbe05f734")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicacionWeb1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicacionWeb1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
